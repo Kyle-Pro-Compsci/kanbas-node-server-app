e8b75d3818a81815e6fe7af8238ba01a3a0cb607
Kyle Koo <kylekoo837@gmail.com>
2023-11-21 20:45:14 -0500

    deployment changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from 'express';
 import Hello from './hello.js';
 import Lab5 from "./lab5.js";
@@ -11,4 +12,4 @@ CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
