e6d17cf9cb56b44b86f5c506a249dd34cfe10cb8
Kyle Koo <kylekoo837@gmail.com>
2023-12-05 13:59:32 -0500

    connection string logging

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ import cors from "cors";
 
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+console.log(`CONNECTION STRING: ${CONNECTION_STRING}`);
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
