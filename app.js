a99855c4f134621455e7691915e7e2781e0abb93
Kyle Koo <kylekoo837@gmail.com>
2023-12-05 07:29:34 -0500

    session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from 'express';
+import session from "express-session";
 import Hello from './hello.js';
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
@@ -11,7 +12,21 @@ import UserRoutes from "./users/routes.js";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(
+  session(sessionOptions)
+);
+
 app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
